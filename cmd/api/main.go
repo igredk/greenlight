32aa325375ecbff71be83aded87196d5f588aea0
Редькин Игорь <igredk@gmail.com>
2024-09-07 12:43:20 +0200

    Implement db connection pull.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const version string = "1.0.0"
@@ -14,6 +18,11 @@ const version string = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -26,10 +35,26 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "pg-dsn", "postgres://greenlight:pa55word@localhost:5432/greenlight", "PostgreSQL connection URL")
+	flag.IntVar(&cfg.db.maxOpenConns, "pg-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "pg-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	// Initialize a new structured logger which writes log entries to the standard out stream.
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	dbPool, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	defer dbPool.Close()
+	logger.Info("Database connection established")
+	logger.Info(
+		"Database connections pool config",
+		"max open connections", strconv.Itoa(int((dbPool.Config().MaxConns))),
+		"connection idle time", dbPool.Config().MaxConnIdleTime.String(),
+	)
 
 	app := &application{
 		config: cfg,
@@ -45,12 +70,38 @@ func main() {
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
 	// Start the HTTP server.
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config, err := pgxpool.ParseConfig(cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	config.MaxConns = int32(cfg.db.maxOpenConns)
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	config.MaxConnIdleTime = duration
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+	return pool, nil
 }
