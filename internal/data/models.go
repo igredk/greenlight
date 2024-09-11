f7cefcfdb4bec3c2cbdf1001a08d46ea9a1b0dcd
Редькин Игорь <igredk@gmail.com>
2024-09-11 20:07:12 +0200

    Implement optimistic locking for PATCH /v1/movies/:id endpoint.

--- internal/data/models.go
+++ internal/data/models.go
@@ -6,10 +6,9 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// Define a custom ErrRecordNotFound error. We'll return this from our Get() method when
-// looking up a movie that doesn't exist in our database.
 var (
 	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
 // Create a Models struct which wraps the MovieModel. We'll add other models to this,
