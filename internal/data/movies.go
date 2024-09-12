3798b6eaccd1e19cbd5bcbc144b808750712a44e
Редькин Игорь <igredk@gmail.com>
2024-09-12 11:35:56 +0200

    Add timeouts for Movie CRUD operations.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -50,7 +50,10 @@ func (m MovieModel) Insert(movie *Movie) error {
 
 	args := []any{movie.Title, movie.Year, movie.Runtime, movie.Genres}
 
-	return m.DB.QueryRow(context.Background(), query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return m.DB.QueryRow(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
@@ -63,7 +66,10 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
         FROM movies
         WHERE id = $1`
 
-	rows, _ := m.DB.Query(context.Background(), query, id)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, _ := m.DB.Query(ctx, query, id)
 
 	movie, err := pgx.CollectOneRow(rows, pgx.RowToAddrOfStructByName[Movie])
 	if err != nil {
@@ -93,7 +99,10 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Version,
 	}
 
-	err := m.DB.QueryRow(context.Background(), query, args...).Scan(&movie.Version)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRow(ctx, query, args...).Scan(&movie.Version)
 	if err != nil {
 		switch {
 		case errors.Is(err, pgx.ErrNoRows):
@@ -115,7 +124,10 @@ func (m MovieModel) Delete(id int64) error {
         DELETE FROM movies
         WHERE id = $1`
 
-	result, err := m.DB.Exec(context.Background(), query, id)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	result, err := m.DB.Exec(ctx, query, id)
 	if err != nil {
 		return err
 	}
