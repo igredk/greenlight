cf9d5f400184c9c1fab31935c0815bc97b5f2bc8
Редькин Игорь <igredk@gmail.com>
2024-10-03 19:35:44 +0200

    Implement /v1/tokens/authentication endpoint.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -25,6 +25,8 @@ func (app *application) routes() http.Handler {
 	// users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activate", app.activateUserHandler)
+	// tokens
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
