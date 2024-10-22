712749468b61004212088b166713fba05276b676
Редькин Игорь <igredk@gmail.com>
2024-10-22 19:30:30 +0200

    Add support for preflight requests in CORS middleware.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -214,20 +214,28 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Add the "Vary: Origin" header to warn any caches that the response may be different.
+		// Add the "Vary" header to warn any caches that the response may be different.
 		w.Header().Add("Vary", "Origin")
-		// Get the value of the request's Origin header.
-		origin := r.Header.Get("Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
+		origin := r.Header.Get("Origin") // Get the value of the request's Origin header.
 
 		if origin != "" {
-			// Loop through the list of trusted origins, checking to see if the request
-			// origin exactly matches one of them. If there are no trusted origins, then
-			// the loop won't be iterated.
 			for i := range app.config.cors.trustedOrigins {
 				if origin == app.config.cors.trustedOrigins[i] {
-					// If there is a match, then set a "Access-Control-Allow-Origin"
-					// response header with the request origin as the value.
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					// Check if the request has the HTTP method OPTIONS and contains the
+					// "Access-Control-Request-Method" header. If it does, then we treat it as a preflight request.
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						// Set the necessary preflight response headers.
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+						// Return from the middleware with no further action.
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+
 					break
 				}
 			}
